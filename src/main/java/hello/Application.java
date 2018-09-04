1e928a00f3db38fd2f5285089387403782b93b29
ddorante <Xikitoelmejor_88>
2018-09-04 16:47:40 +0200

    Ultima practica de Spring

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -7,7 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
 
 @SpringBootApplication
 public class Application {
@@ -20,7 +24,13 @@ public class Application {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        SimpleClientHttpRequestFactory clientHttpReq = new SimpleClientHttpRequestFactory();
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.indra.es", 8080));
+        clientHttpReq.setProxy(proxy);
+
+        RestTemplate restTemplate = new RestTemplate(clientHttpReq);
+        return restTemplate;
+        //return builder.build();
     }
 
     @Bean
